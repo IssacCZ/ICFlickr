a0a5c5023004893517638e426d00c2c28b550dac
IssacCZ <sheldowkay@icloud.com>
2016-01-05 20:03:38 +0800

    新增collectionView

--- ICFlickr/Codes/ViewController.swift
+++ ICFlickr/Codes/ViewController.swift
@@ -8,23 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor.blackColor()
-        let imageView:UIImageView = UIImageView()
-        imageView.frame = view.bounds
-        imageView.image = UIImage(named: "Icon-180")
-        imageView.contentMode = UIViewContentMode.ScaleAspectFit
-        view.addSubview(imageView)
-        
-        print("屏幕高度是：\(AppUtil.currentHeight()), 宽度是：\(AppUtil.currentWidth())");
+        initUI()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    func initUI() {
+        view.backgroundColor = UIColor.whiteColor()
+        UIApplication.sharedApplication().statusBarStyle = .LightContent
+        let layout = UICollectionViewFlowLayout()
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
+        collectionView!.delegate = self
+        collectionView!.dataSource = self
+        collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.reloadData()
+        collectionView.backgroundColor = UIColor.whiteColor()
+        view.addSubview(collectionView);
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath)
+//        cell.frame = CGRect(x: 0, y: 0, width: (AppUtil.currentWidth() - 2 * 4) / 3, height: (AppUtil.currentWidth() - 2 * 4) / 3)
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.frame = cell.bounds
+        imageView.image = UIImage(named: "Icon-1024")
+        imageView.contentMode = UIViewContentMode.ScaleAspectFit
+        cell.addSubview(imageView)
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 20, left: 2, bottom: 2, right: 2)
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 2
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 2
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSize(width: (AppUtil.currentWidth() - 2 * 4) / 3, height: (AppUtil.currentWidth() - 2 * 4) / 3)
+    }
 }
-
